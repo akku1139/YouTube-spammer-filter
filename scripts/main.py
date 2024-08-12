0b71672151980fda3e20a4a74ce36c32bf94ae01
-akku- <151517253+akku1139@users.noreply.github.com>
2024-08-13 01:44:56 +0900

    Create main.py

---
+++ scripts/main.py
@@ -0,0 +1,11 @@
+import urllib.parse
+
+out = open("./filter.txt", mode="w")
+
+with open("./src/channels.txt") with src:
+  for line in src:
+    l = "www.youtube.com##a[href=\"/" + urllib.parse.quote("@純白の天使ラフメシア_3".removeprefix("/"), safe="@") + "\"]:upward(ytd-comment-view-model)"
+    print(line, ":", l)
+    out.write(l + "\n")
+
+out.close()
