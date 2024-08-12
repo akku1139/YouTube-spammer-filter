5e24b484909858d23f0d655cf482d2fc4a9ef02a
-akku- <151517253+akku1139@users.noreply.github.com>
2024-08-13 01:52:59 +0900

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,7 @@ out = open("./filter.txt", mode="w")
 
 with open("./src/channels.txt") as src:
   for line in src:
-    l = "www.youtube.com##a[href=\"/" + urllib.parse.quote(line.removeprefix("/"), safe="@") + "\"]:upward(ytd-comment-view-model)"
+    l = "www.youtube.com##a[href=\"/" + urllib.parse.quote(line.removeprefix("/").removesuffix("\n"), safe="@") + "\"]:upward(ytd-comment-view-model)"
     print(line, ":", l)
     out.write(l + "\n")
 
