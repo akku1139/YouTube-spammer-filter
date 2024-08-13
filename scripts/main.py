eb65670c6b45cd838e287cb68b76c711511b53eb
-akku- <151517253+akku1139@users.noreply.github.com>
2024-08-14 01:37:07 +0900

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -6,15 +6,18 @@ def out(text):
   outfile.write(text + "\n")
 
 out("! spammer channels")
-
 with open("./src/channels.txt") as src:
   for line in src:
     out("www.youtube.com##a[href=\"/" + urllib.parse.quote(line.removeprefix("/").removesuffix("\n"), safe="@") + "\"]:upward(6)")
 
 out("\n! spam words")
-
 with open("./src/words.txt") as src:
   for line in src:
     out("###content-text>span:has-text(/" + line + "/):upward(5)")
+
+out("\n! template comments")
+with open("./src/templates.txt") as src:
+  for line in src:
+    out("###content-text>span:has-text(" + line + "):upward(5)")
 
 outfile.close()
