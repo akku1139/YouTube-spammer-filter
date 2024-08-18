6ac82a79d5442989e064fa86c36190edda42c6b0
-akku- <151517253+akku1139@users.noreply.github.com>
2024-08-19 01:33:22 +0900

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -5,9 +5,9 @@ outfile = open("./filter.txt", mode="w")
 def out(text):
   return outfile.write(text + "\n")
 
-def encodeURI(text):
+def encodeURI(text, safe=""):
   # Pythonは '_.-~' をデフォルトでエンコードしません
-  return urllib.parse.quote(text, safe=";,/?:@&=+$!*'()#")
+  return urllib.parse.quote(text, safe=";,/?:@&=+$!*'()#"+safe)
 
 out("! Homepage: https://github.com/akku1139/YouTube-spammer-filter")
 out("! Title: YouTube spam comment filter for Japanese")
@@ -24,7 +24,7 @@ def make(filename, comment, func):
 
 make("channels.txt", "spammer channels", lambda line: ("a[href=\"/" + encodeURI(line.removeprefix("/")) + "\"]:upward(6)"))
 
-make("channel-regex.txt", "spammer channels (with regex)", lambda line: ("a[href=/\/" + encodeURI(line.removeprefix("/")) + "/]:upward(6)"))
+make("channel-regex.txt", "spammer channels (with regex)", lambda line: ("a[href=/\/" + encodeURI(line.removeprefix("/"), safe="^\\") + "/]:upward(6)"))
 
 make("words.txt", "spam words", lambda line: ("#content-text>span:has-text(/" + line + "/):upward(5)"))
 
