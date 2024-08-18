8bd7966d15b4a3630fb87956e796b3ad8be3bce6
-akku- <151517253+akku1139@users.noreply.github.com>
2024-08-19 01:30:51 +0900

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -3,7 +3,11 @@ import urllib.parse
 outfile = open("./filter.txt", mode="w")
 
 def out(text):
-  outfile.write(text + "\n")
+  return outfile.write(text + "\n")
+
+def encodeURI(text):
+  # Pythonは '_.-~' をデフォルトでエンコードしません
+  return urllib.parse.quote(text, safe=";,/?:@&=+$!*'()#")
 
 out("! Homepage: https://github.com/akku1139/YouTube-spammer-filter")
 out("! Title: YouTube spam comment filter for Japanese")
@@ -18,9 +22,9 @@ def make(filename, comment, func):
         continue
       out("www.youtube.com###sections " + func(line.strip()))
 
-make("channels.txt", "spammer channels", lambda line: ("a[href=\"/" + urllib.parse.quote(line.removeprefix("/").removesuffix("\n"), safe="@") + "\"]:upward(6)"))
+make("channels.txt", "spammer channels", lambda line: ("a[href=\"/" + encodeURI(line.removeprefix("/")) + "\"]:upward(6)"))
 
-make("channel-regex.txt", "spammer channels (with regex)", lambda line: ("a[href=/\/" + urllib.parse.quote(line.removeprefix("/").removesuffix("\n"), safe="@") + "/]:upward(6)"))
+make("channel-regex.txt", "spammer channels (with regex)", lambda line: ("a[href=/\/" + encodeURI(line.removeprefix("/")) + "/]:upward(6)"))
 
 make("words.txt", "spam words", lambda line: ("#content-text>span:has-text(/" + line + "/):upward(5)"))
 
