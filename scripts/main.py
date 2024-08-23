93e0f1e386c7cf3d06596d5951a789a5ca14144f
-akku- <151517253+akku1139@users.noreply.github.com>
2024-08-24 00:21:33 +0900

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,8 @@
 import urllib.parse
 import urllib.request
+import urllib.error
 import json
+from logging import getLogger
 from bs4 import BeautifulSoup
 
 # データを読み込む
@@ -32,24 +34,32 @@ def make(filename, comment, func):
         continue
       if line.startswith("#"):
         continue
-      out("www.youtube.com###sections " + func(line.strip()))
+      ret = func(line.strip())
+      if ret == "":
+        return
+      out("www.youtube.com###sections " + ret)
 
 make("channels.txt", "Spammer channels", lambda line: ("a[href=\"/" + encodeURI(line.removeprefix("/")) + "\"]:upward(6)"))
 
 # 多分 querySelector() の属性Selectorでは正規表現が使えない
+# 無駄に正規表現使うのは重いので前方一致とかで良いと思う
 # make("channel-regex.txt", "spammer channels (with regex)", lambda line: ("a[href=/\/" + encodeURI(line.removeprefix("/"), safe="^\\") + "/]:upward(6)"))
 
 def make_reply_filter(line):
   if line not in channel_id:
-    # エラーが起きない前提のコード
-    url = "https://youtube.com/"+encodeURI(line)
-    print(url)
-    res = fetch(url)
-    soup = BeautifulSoup(res, 'html.parser')
-    channel_id[line] = soup.find("meta", itemprop="identifier")["content"]
+    try:
+      # 429とかのエラーが起きない前提のコード
+      res = fetch("https://youtube.com/"+encodeURI(line))
+      soup = BeautifulSoup(res, 'html.parser')
+      channel_id[line] = soup.find("meta", itemprop="identifier")["content"]
+    except urllib.error.HTTPError as e:
+      if e.code == 404:
+        logger.warning("Channel not found: "+line)
+      raise # if not 404 (eを再度投げる必要はない)
 
   return "##a[href=\"/channel/"+channel_id[line]+"\"]:upward(8)"
-
+  
+logger = getLogger("reply")
 make("channels.txt", "Reply to spammers", make_reply_filter)
 
 make("words.txt", "Spam words", lambda line: ("#content-text>span:has-text(/" + line + "/):upward(5)"))
